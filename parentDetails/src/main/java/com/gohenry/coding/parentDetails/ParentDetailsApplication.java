c3f3cffae97b62018e248478373afadc84ad2141
Jisha <jisha.sainu@gmail.com>
2019-08-30 10:25:04 +0100

    initial commit

---
+++ parentDetails/src/main/java/com/gohenry/coding/parentDetails/ParentDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.gohenry.coding.parentDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentDetailsApplication.class, args);
+	}
+
+}
